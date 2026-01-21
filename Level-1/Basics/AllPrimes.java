e3859b69b06df1470bec3ead45f0e38652d5e9fd
Tirth <tirthcoder@gmail.com>
2026-01-21 12:43:13 +0530

    Basics

---
+++ Level-1/Basics/AllPrimes.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AllPrimes {
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    int lo = scn.nextInt();
+    int hi = scn.nextInt();
+    for(int i = lo; i <= hi; i++){
+        int count = 0;
+        for(int div = 2; div * div <= i; div++){
+            if(i % div == 0){
+                count++;
+                break;
+            }
+        }
+
+        if(count == 0) System.out.println(i);
+    }
+  }
+}
---
+++ Level-1/Basics/IsPrime.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class IsPrime {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int t = scn.nextInt();
+        while (t-- > 0) {
+            int n = scn.nextInt();
+            int count = 0;
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) {
+                    count++;
+                    break;
+                }
+            }
+
+            if (count == 0)
+                System.out.println("prime");
+            else
+                System.out.println("not prime");
+        }
+    }
+}
