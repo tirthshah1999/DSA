3d6c8882a27206b344884661e0c5b3110a7676a0
tirth <tirthcoder@gmail.com>
2026-02-14 15:52:17 +0530

    Patterns

---
+++ Level-1/Patterns/P16.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class P16 {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        int st = 1, sp = 2 * n - 3;
+
+        for (int i = 1; i <= n; i++) {
+            int val = 1;
+            for (int j = 1; j <= st; j++) {
+                System.out.print(val + "\t");
+                val++;
+            }
+            for (int j = 1; j <= sp; j++) {
+                System.out.print("\t");
+            }
+            if (i == n) {
+                st--;
+                val--;
+            }
+            for (int j = 1; j <= st; j++) {
+                val--;
+                System.out.print(val + "\t");
+            }
+            st++;
+            sp -= 2;
+            System.out.println("");
+        }
+    }
+}
