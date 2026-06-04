ebedc4be7239561fb4aa481dc0558749ac0b5009
Tirth <tirthcoder@gmail.com>
2026-06-04 12:14:13 +0530

    Recursion

---
+++ Level-1/Recursion/PrintZigZag.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PrintZigZag {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        pzz(n);
+    }
+
+    public static void pzz(int n) {
+        if (n == 0)
+            return;
+        System.out.print(n + " ");
+        pzz(n - 1);
+        System.out.print(n + " ");
+        pzz(n - 1);
+        System.out.print(n + " ");
+    }
+}
---
+++ Level-1/Recursion/TowerOfHanoi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class TowerOfHanoi {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int t1d = scn.nextInt();
+        int t2d = scn.nextInt();
+        int t3d = scn.nextInt();
+        toh(n, t1d, t2d, t3d);
+    }
+
+    public static void toh(int n, int t1id, int t2id, int t3id) {
+        if (n == 0)
+            return;
+        toh(n - 1, t1id, t3id, t2id);
+        System.out.println(n + "[" + t1id + " -> " + t2id + "]");
+        toh(n - 1, t3id, t2id, t1id);
+    }
+}
